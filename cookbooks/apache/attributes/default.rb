a1c7f2f4dbba6bac8f24b10d24d7e853ec37db3d
root <root@sgaire1.mylabserver.com>
2016-04-11 00:59:31 +0000

    New chef-repository commited

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,3 @@
+default["apache"]["sites"]["sgaire2"] = { "port" => 80, "domain" => "sgaire1.mylabserver.com"}
+default["apache"]["sites"]["sgaire2b"] = { "port" => 80, "domain" => "sgaire2b.mylabserver.com"}
+ 
